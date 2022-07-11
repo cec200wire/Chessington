bd7928ecda8d840f7b6e8547700ff111d45d25ca
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-11 11:26:29 +0100

    Test group 5 (queen) complete

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Controls.Ribbon.Primitives;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +12,30 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> validMoves = new List<Square>();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    int displaceRow = currentSquare.Row - i;
+                    int displaceCol = currentSquare.Col - j;
+
+                    if (displaceRow != 0 || displaceCol != 0)
+                    {
+                        if (displaceRow * displaceCol == 0)
+                        {
+                            validMoves.Add(Square.At(i, j));
+                        }
+
+                        else if (Math.Abs(displaceRow) == Math.Abs(displaceCol))
+                        {
+                            validMoves.Add(Square.At(i, j));
+                        }
+                    }
+                }
+            }
+            return validMoves;
         }
     }
 }
