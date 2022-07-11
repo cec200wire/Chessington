4f41e276b43ea1db7ff7763abd6492893c9dffde
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-11 11:14:06 +0100

    Test group 4 (bishop) complete

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> validMoves = new List<Square>();
+            List<Square> potentialMoves = new List<Square>();
+            for (int i = 1; i < 8; i++)
+            {
+                potentialMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col + i));
+                potentialMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col - i));
+                potentialMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col + i));
+                potentialMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col - i));
+            }
+
+            foreach (Square potentialMove in potentialMoves){
+                if ((-1 < potentialMove.Row) & (potentialMove.Row < 8) & (-1 < potentialMove.Col) & (potentialMove.Col < 8))
+                {
+                    validMoves.Add(potentialMove);
+                }
+            }
+            return validMoves;
         }
     }
 }
